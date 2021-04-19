ed7df974b092a01ddb754b6635f5eccd8ea1c304
Maverick <maverick.champi@gmail.com>
2021-04-18 22:18:35 -0500

    Updates

---
+++ precache-manifest.b30aca775ed14edc7acdc06fddebc378.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc1bea2278a6f3de5d2e",
+    "url": "/moonConf/static/css/main.7a26104d.chunk.css"
+  },
+  {
+    "revision": "fc1bea2278a6f3de5d2e",
+    "url": "/moonConf/static/js/main.fc1bea22.chunk.js"
+  },
+  {
+    "revision": "8595d7ce3e59691fef50",
+    "url": "/moonConf/static/js/runtime~main.8595d7ce.js"
+  },
+  {
+    "revision": "63999c161d419fd6dbce",
+    "url": "/moonConf/static/css/2.ef8be977.chunk.css"
+  },
+  {
+    "revision": "63999c161d419fd6dbce",
+    "url": "/moonConf/static/js/2.63999c16.chunk.js"
+  },
+  {
+    "revision": "24373f60fc126f97a317f1acfc4fac16",
+    "url": "/moonConf/static/media/astronauta.24373f60.svg"
+  },
+  {
+    "revision": "bb799883ccfb6b5adc27f69fee2206eb",
+    "url": "/moonConf/static/media/eclipse.bb799883.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/moonConf/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "6114351a9121e04756edaf99cbfc5f24",
+    "url": "/moonConf/static/media/Astronaut-bro.6114351a.svg"
+  },
+  {
+    "revision": "53a129fc7dc2436a94150589af1d10f3",
+    "url": "/moonConf/static/media/Outer space-bro.53a129fc.svg"
+  },
+  {
+    "revision": "2ba8a27f4f66af3f6039336eff71c67d",
+    "url": "/moonConf/static/media/luna.2ba8a27f.svg"
+  },
+  {
+    "revision": "ecdc4c14cb2550a8c30d8853bb2a6258",
+    "url": "/moonConf/index.html"
+  }
+];
